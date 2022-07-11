7473fc8730c9ec2c558045cc9309d25a13610238
TJ <thomasjjohnson9311@gmail.com>
2022-07-11 17:34:37 -0500

    map through array & return table row for each object in the array

--- src/Table.js
+++ src/Table.js
@@ -2,18 +2,26 @@ import React, { Component } from 'react'
 
 const TableHeader = () => {
     return (
-        <thead>
+        
             <tr>
                 <th>Name</th>
                 <th>Job</th>
             </tr>
-        </thead>
+        
     )
 }
 
-const TableBody = () => {
+const TableBody = (props) => {
+    const rows = props.characterData.map((row, index) => {
+        return (
+            <tr key={index}>
+                <td>{row.name}</td>
+                <td>{row.job}</td>
+            </tr>
+        )
+    })
     return (
-        <tbody />
+        <tbody>{rows} </tbody>
     )
 }
 
